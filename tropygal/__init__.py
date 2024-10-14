43a097d4c986e2c176a151eae6e836247e8785ad
Leandro Beraldo e Silva <lberaldoesilva@gmail.com>
2024-10-14 16:10:06 -0700

    Cleaning non-polished code

--- tropygal/__init__.py
+++ tropygal/__init__.py
@@ -1,4 +1,4 @@
 """ tropygal: entropy estimators for galactic dynamics"""
-from ._base_funcs import V_d, entropy, cross_entropy, C_k, renyi_entropy, tsallis_entropy
+from .base_funcs import V_d, entropy, cross_entropy, C_k, renyi_entropy, tsallis_entropy
 from .DFs import DF_Isochrone, g_Isochrone, gEL_Isochrone, gEL_Spherical
 # from . import _base_funcs
